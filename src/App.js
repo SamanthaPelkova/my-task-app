00788c5616d435cbc36d0c72bb64970edf14e659
Samantha Pelková <102859715+SamanthaPelkova@users.noreply.github.com>
2023-07-09 20:42:52 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AllTasks from "./components/alltasks";
+
+const App = () => {
+  return (
+    <div className="all-tasks">
+      <AllTasks></AllTasks>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/alltasks.js
@@ -0,0 +1,36 @@
+import "./alltasks.css";
+import data from "../data"
+import {useState} from "react"
+
+const AllTasks = () => {
+    const [myTask, setMyTask] = useState(data)
+
+    const tasksHandler = (idecko) => {
+        const filteredTasks = myTask.filter((oneTask) => {
+            return oneTask.id !==idecko
+        })
+        setMyTask(filteredTasks);
+    }
+
+    const allDeleteHandler = () => {
+        setMyTask([])
+    }
+    
+
+  return <div>
+    {
+        myTask.map((oneTask) => {
+            const {id, name} = oneTask
+            return <div className="one-task" key={id} >
+                <h4>{name}</h4>
+                <button onClick={() => tasksHandler(id)} > Vymazat </button>
+            </div>
+        })
+    }
+    <button className="main-button" onClick={allDeleteHandler}> Vymazat vše </button>
+
+
+  </div>;
+};
+
+export default AllTasks;
